d227e4e4e00032b3a298bd9b116e22694d300c04
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-09 03:29:40 +0000

    Used pass by reference techniques and methodologies to manipulate my Object.

--- index.js
+++ index.js
@@ -19,3 +19,28 @@
   console.log(schedule);
 
   console.log( isEmpty(schedule) ); 
+
+// ----------------------------------------------------
+
+
+/* Write a function greenEnergy() that has an object as a parameter and sets that object’s 'Fuel Type' property to 'avocado oil'.  */ 
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+let greenEnergy = obj => {
+   obj['Fuel Type'] = 'avocado oil'
+}
+/* Write a function remotelyDisable() that has an object as a parameter and sets (or reassigns) that object’s disabled property to true. */ 
+
+let remotelyDisable = spaceship => {
+   spaceship.disabled = true;
+};
+ greenEnergy(spaceship);
+// This is how i called my 2 func()
+ remotelyDisable(spaceship);
+ console.log(spaceship);
+//  Used console to confirm properties were changed/added.
